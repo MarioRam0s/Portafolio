df187c74a0b28e181098d1ed9f7aa8d406c9b75d
MarioRam0s <marioramosmejia2243@gmail.com>
2025-05-17 10:16:50 +0200

    feat: Agregando seccion de habilidades y eliminando la carpeta de contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./components/header/header.component";
-import { AboutComponent } from "./components/about/about.component";
-import { SkillsComponent } from "./components/skills/skills.component";
-import { ContactComponent } from "./components/contact/contact.component";
-import { EducationComponent } from "./components/education/education.component";
-import { ExperienceComponent } from "./components/experience/experience.component";
-import { GoalsComponent } from "./components/goals/goals.component";
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { GoalsComponent } from './components/goals/goals.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, AboutComponent, SkillsComponent, ContactComponent, EducationComponent, ExperienceComponent, GoalsComponent],
+  imports: [
+    HeaderComponent,
+    AboutComponent,
+    SkillsComponent,
+    EducationComponent,
+    ExperienceComponent,
+    GoalsComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Portafolio';
